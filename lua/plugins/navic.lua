1e5e5893599ef7304c26d9269f5c7e296ed6870a
AaronRan <aaronran07@outlook.com>
2023-12-16 15:01:58 +0800

    fix deprected 'on_attach' invoke

--- lua/plugins/navic.lua
+++ lua/plugins/navic.lua
@@ -4,7 +4,7 @@ return {
     lazy = true,
     init = function()
       vim.g.navic_silence = false
-      require("lazyvim.util").on_attach(function(client, buffer)
+      require("lazyvim.util").lsp.on_attach(function(client, buffer)
         if client.server_capabilities.documentSymbolProvider then
           require("nvim-navic").attach(client, buffer)
         end
