a991ed873dcbaaaea211cad469fab712bcd67e05
AaronRan <aaronran07@outlook.com>
2023-12-16 13:03:05 +0800

    add keymap '<leader>ba' to save all buffers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- save changed buffers
+local Util = require("lazyvim.util")
+local map = Util.safe_keymap_set
+local unmap = vim.keymap.del
+map("n", "<leader>ba", "<cmd>wa<cr>", { desc = "Save all buffers" })
